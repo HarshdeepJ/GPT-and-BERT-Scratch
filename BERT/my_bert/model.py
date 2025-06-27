0fbe1602a1ebaad036dc645f98f75efb163b8fca
HarshdeepJ <111578177+HarshdeepJ@users.noreply.github.com>
2025-06-27 21:02:12 +0530

    model.py modified

--- BERT/my_bert/model.py
+++ BERT/my_bert/model.py
@@ -26,3 +26,31 @@ class BertModel(nn.Module):
             x = layer(x, mask = attention_mask)
         
         return x
+    
+class BertForPreTraining(nn.Module):
+    def __init__(self, vocab_size, d_model, num_layers, num_heads, d_ff, max_len, dropout_prob):
+        super().__init__()
+        self.bert = BertModel(
+            vocab_size = vocab_size,
+            d_model = d_model,
+            num_layers = num_layers,
+            num_heads = num_heads,
+            d_ff = d_ff,
+            max_len = max_len,
+            dropout_prob = dropout_prob,
+        )
+
+        self.nsp_head = nn.Sequential(
+            nn.Linear(d_model, d_model),
+            nn.Tanh(),
+            nn.Linear(d_model, 2),
+        )
+
+        self.mlm_head = nn.Linear(d_model, vocab_size)
+    
+    def forward(self, input_ids, segment_ids, attention_mask = None):
+        sequence_output = self.bert(input_ids, segment_ids, attention_mask)
+        cls_token_output = sequence_output[:, 0]
+        nsp_logits = self.nsp_head(cls_token_output)
+        mlm_logits = self.mlm_head(sequence_output)
+        return mlm_logits, nsp_logits
